e0aee6716c9c4b1d3c3940c1c999e6a98b43b33a
jinsheng <sheng.jin@8win.com>
2017-05-16 17:30:20 +0800

    spring boot security init

---
+++ spring-boot-security-example/src/main/java/com/jinsheng/spring/boot/security/SpringBootSecurityApplication.java
@@ -0,0 +1,15 @@
+package com.jinsheng.spring.boot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jinsheng
+ */
+@SpringBootApplication
+public class SpringBootSecurityApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootSecurityApplication.class, args);
+    }
+}
